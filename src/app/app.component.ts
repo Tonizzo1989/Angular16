ce89b3058f46f4b262b81fa2c79cda44b9c8f2d0
Tonino <toninorizzo1989@yahoo.it>
2024-10-02 10:37:18 +0200

    added ngSwitchCase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng16';
+  isVisible = true;
+
+  people = [
+    {
+      name: 'Paul',
+      surname: 'Badwihite',
+      age: '22',
+      isOnline: false,
+    },
+    {
+      name: 'Chris',
+      surname: 'Lion',
+      age: '32',
+      isOnline: true,
+    },
+    {
+      name: 'Maggie',
+      surname: 'Woolrich',
+      age: '28',
+      isOnline: false,
+    },
+    {
+      name: 'Sara',
+      surname: 'Castellani',
+      age: '35',
+      isOnline: true,
+    },
+    {
+      name: 'Roberto',
+      surname: 'Casadei',
+      age: '29',
+      isOnline: false,
+    },
+  ];
+
+  
+  string = "minnie";
 
   onInput(event: Event) {
-    console.log((<HTMLInputElement>event.target).value);
+    this.title = (<HTMLInputElement>event.target).value;
   }
+
+  onClick(event: Event) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProvaComponent } from './prova/prova.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 
@@ -19,6 +22,7 @@ import { MatInputModule } from '@angular/material/input';
     MatCardModule,
     MatButtonModule,
     MatInputModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
